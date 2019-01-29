c53f9b5697121a066486c18dba0a8dc6265cc243
Wei Jian <weijian0955@126.com>
2019-01-29 16:52:59 +0800

    add price_spider app

---
+++ price_spider/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PriceSpiderConfig(AppConfig):
+    name = 'price_spider'
---
+++ price_spider/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.utils.translation import ugettext as _
+
+# Create your models here.
+class Goods(models.Model):
+    name =  models.CharField(max_length=20, verbose_name=_('商品名称'))
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=10, verbose_name=_('商品类型'))
+
+
+class Unit(models.Model):
+    name = models.CharField(max_length=10, verbose_name=_('单位'))
+
+
+class LatestRecord(models.Model):
+    date = models.DateField(verbose_name=_('最新数据日期'))
+
+
+class Price(models.Model):
+    date = models.DateField(verbose_name=_('数据日期'))
+    goods = models.ForeignKey(Goods, verbose_name=_('商品'))
+    category = models.ForeignKey(Category, verbose_name=_('商品类型'))
+    unit = models.ForeignKey(Unit, verbose_name=_('单位'))
+    price = models.FloatField(verbose_name=_('均价'))
